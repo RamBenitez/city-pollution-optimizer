16e91e435cca2d7d47eae076e56f645d5e944798
RamBenitez <ramilimanbenitez21@gmail.com>
2025-12-01 23:44:38 +0800

    added app.jsx

--- frontend/city-pollution-optimizer/src/App.jsx
+++ frontend/city-pollution-optimizer/src/App.jsx
@@ -1,19 +1,18 @@
 // App.jsx
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import LandingPage from "./pages/landingpage";
 import LandingPage from "./pages/LandingPage";
 import SolverPage from "./pages/SolverPage";
 import "./App.css";
 
 function App() {
   return (
-    <router>
+  
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/projects" element={<SolverPage />} />
         <Route path="/solver" element={<SolverPage />} />
       </Routes>
-    </router>
+  
   );
 }
 
